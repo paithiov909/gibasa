bae415662e045eced520ae9570d819159fda9cc1
paithiov909 <am11.44@gmail.com>
2022-03-26 16:56:46 +0900

    feat: UniDic support

---
+++ R/utils-unidic.R
@@ -0,0 +1,37 @@
+#' List of available 'UniDic'
+#' @export
+unidic_availables <- function() {
+  jsonlite::read_json(
+    "https://raw.githubusercontent.com/polm/unidic-py/master/dicts.json"
+  )
+}
+
+#' Download and unzip 'UniDic'
+#'
+#' Download 'UniDic' of specified version into `dirname`.
+#' This function is partial port of \href{https://github.com/polm/unidic-py}{polm/unidic-py}.
+#' Note that to unzip dictionary will take up 770MB on disk after downloading.
+#'
+#' @param version String; version of 'UniDic'.
+#' @param dirname String; directory where unzip the dictionary.
+#' @return full path to `dirname` is returned invisibly.
+#' @export
+download_unidic <- function(version = "latest", dirname = "unidic") {
+  json <- unidic_availables()
+  version <- rlang::arg_match(version, values = names(json))
+
+  if (yesno::yesno(sprintf("Download UniDic version %s ?", version))) {
+    rlang::inform(
+      sprintf("Downloading UniDic version %s ...", version)
+    )
+    temp <- tempfile(fileext = ".zip")
+    utils::download.file(json[[version]]$url, temp)
+    utils::unzip(temp, exdir = file.path(dirname))
+  }
+  on.exit(
+    rlang::inform(
+      sprintf("Downloaded UniDic version %s", version)
+    )
+  )
+  return(invisible(file.path(dirname)))
+}
