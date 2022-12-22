7f702976d9dcfb63230c266462ac19468198a1db
paithiov909 <am11.44@gmail.com>
2022-12-23 08:59:34 +0900

    docs: revert missing examples

--- R/bind_tf_idf2.R
+++ R/bind_tf_idf2.R
@@ -4,4 +4,17 @@
 #' @inheritParams audubon::bind_tf_idf2
 #' @importFrom audubon bind_tf_idf2
 #' @export
+#' @examples
+#' \dontrun{
+#' df <- tokenize(
+#'   data.frame(
+#'     doc_id = seq_along(audubon::polano[5:8]),
+#'     text = audubon::polano[5:8]
+#'   )
+#' ) |>
+#'   dplyr::group_by(doc_id) |>
+#'   dplyr::count(token) |>
+#'   dplyr::ungroup()
+#' bind_tf_idf2(df)
+#' }
 bind_tf_idf2 <- audubon::bind_tf_idf2
